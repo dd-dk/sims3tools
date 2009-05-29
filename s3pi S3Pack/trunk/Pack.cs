87b789f9cb813e2dc0a05b405c09a292d517d2f9
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2009-05-29 22:13:50 +0000

    Initial commit

---
+++ s3pi S3Pack/trunk/Pack.cs
@@ -0,0 +1,74 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2009 by Peter L Jones                                    *
+ *  peter@users.sf.net                                                     *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace S3Pack
+{
+    public partial class Pack : Form
+    {
+        bool haveSource = false;
+        bool haveTarget = false;
+        public Pack()
+        {
+            InitializeComponent();
+        }
+
+        private void Pack_Shown(object sender, EventArgs e)
+        {
+            btnSource_Click(null, null);
+            btnTarget_Click(null, null);
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = tbSource.Text;
+            if (folderBrowserDialog1.ShowDialog() != DialogResult.OK) return;
+            haveSource = true;
+            btnOK.Enabled = haveSource && haveTarget;
+            tbSource.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnTarget_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog2.SelectedPath = tbTarget.Text;
+            if (folderBrowserDialog2.ShowDialog() != DialogResult.OK) return;
+            haveTarget = true;
+            btnOK.Enabled = haveSource && haveTarget;
+            tbTarget.Text = folderBrowserDialog2.SelectedPath;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            S3Pack.Sims3Pack.Pack(folderBrowserDialog1.SelectedPath, folderBrowserDialog2.SelectedPath);
+            tbSource.Text = tbTarget.Text = "";
+            btnOK.Enabled = haveSource = haveTarget = false;
+            //MainForm_Shown(null, null);
+        }
+    }
+}
---
+++ s3pi S3Pack/trunk/makedist-s3pack.bat
@@ -0,0 +1,48 @@
+@echo off
+set TargetName=S3Pack
+set ConfigurationName=Release
+set base=%TargetName%
+rem -%ConfigurationName%
+set src=%TargetName%-Source
+
+set out=S:\PJSE_etc\s3pi\
+
+set mydate=%date: =0%
+set dd=%mydate:~0,2%
+set mm=%mydate:~3,2%
+set yy=%mydate:~8,2%
+set mytime=%time: =0%
+set h=%mytime:~0,2%
+set m=%mytime:~3,2%
+set s=%mytime:~6,2%
+set suffix=%yy%%mm%-%dd%-%h%%m%
+
+set nsisv=/V3
+
+if x%ConfigurationName%==xRelease goto REL
+set pdb=
+goto noREL
+:REL:
+set pdb=-xr!*.pdb
+:noREL:
+
+
+rem there shouldn't be any to delete...
+del %out%%TargetName%*%suffix%.*
+
+pushd ..
+7za a -r -t7z -mx9 -ms -xr!.?* -xr!*.suo -xr!zzOld -xr!bin -xr!obj -xr!Makefile -xr!*.Config "%out%%src%_%suffix%.7z" "s3pi S3Pack"
+popd
+
+pushd bin\%ConfigurationName%
+echo %suffix% >%TargetName%-Version.txt
+attrib +r %TargetName%-Version.txt
+7za a -r -t7z -mx9 -ms -xr!.?* -xr!pack.exe -xr!unpack.exe -xr!*vshost* -xr!*.Config %pdb% "%out%%base%_%suffix%.7z" *
+del /f %TargetName%-Version.txt
+popd
+
+7za x -o"%base%-%suffix%" "%out%%base%_%suffix%.7z"
+"%PROGRAMFILES%\nsis\makensis" "/DTARGET=%base%-%suffix%" %nsisv% mknsis.nsi "/XOutFile %out%%base%_%suffix%.exe"
+
+rmdir /s/q %base%-%suffix%
+pause
