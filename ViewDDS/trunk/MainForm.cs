05e48d28667c2b7b09782afd41a9233f9c920bec
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2009-06-10 12:13:18 +0000

    Initial commit

---
+++ ViewDDS/trunk/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ViewDDS
+{
+    public partial class MainForm : Form
+    {
+        DdsFileTypePlugin.DdsFile ddsFile = new DdsFileTypePlugin.DdsFile();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            MemoryStream ms = Clipboard.GetData(DataFormats.Serializable) as MemoryStream;
+            if (ms == null)
+                throw new Exception("Clipboard data not a MemoryStream");
+
+            try
+            {
+                Application.UseWaitCursor = true;
+                ddsFile.Load(ms);
+            }
+            finally { Application.UseWaitCursor = false; }
+        }
+
+        private void ckb_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Enabled = false;
+                Application.UseWaitCursor = true;
+                pictureBox1.Image = ddsFile.Image(ckbR.Checked, ckbG.Checked, ckbB.Checked, ckbA.Checked);
+                pictureBox1.Size = pictureBox1.Image.Size;
+            }
+            finally { this.Enabled = true; Application.UseWaitCursor = false; }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ckb_CheckedChanged(null, null);
+        }
+    }
+}
---
+++ ViewDDS/trunk/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ViewDDS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static int Main(params string[] args)
+        {
+            bool useClipboard = false;
+            bool useFile = false;
+            List<string> files = new List<string>();
+
+            foreach (string s in args)
+            {
+                string p = s;
+                if (p.StartsWith("/") || p.StartsWith("-"))
+                {
+                    if ("clipboard".StartsWith(p.Substring(1).ToLower()))
+                        useClipboard = true;
+                }
+                else
+                    files.Add(s);
+            }
+            useFile = !useClipboard && files.Count > 0;
+
+            if (!useClipboard && !useFile)
+            {
+                MessageBox.Show("Either pass \"/Clipboard\" switch or a filename", "Fail", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return 1;
+            }
+            if (useClipboard && !Clipboard.ContainsData(DataFormats.Serializable))
+            {
+                MessageBox.Show("No suitable data on clipboard", "Fail", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return 2;
+            }
+            if (useFile)
+            {
+                MessageBox.Show("Only pass one file at a time", "Fail", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                if (files.Count > 1) return 3;
+                MemoryStream ms = new MemoryStream();
+                FileStream fs = new FileStream(files[0], FileMode.Open, FileAccess.Read);
+                (new BinaryWriter(ms)).Write((new BinaryReader(fs)).ReadBytes((int)fs.Length));
+                fs.Close();
+                ms.Flush();
+                Clipboard.SetData(DataFormats.Serializable, ms.GetBuffer());
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                Application.Run(new MainForm());
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+                for (Exception inex = ex.InnerException; inex != null; inex = ex.InnerException) s += "\n" + inex.Message;
+                    MessageBox.Show(s, "Fail", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
