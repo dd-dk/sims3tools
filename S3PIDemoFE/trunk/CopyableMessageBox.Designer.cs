d276c14eb72283392ae7b5192b9dedbb9ccdbb0e
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2009-06-28 12:42:11 +0000

    Create a new message box that supports copying the text (and resizing).

---
+++ S3PIDemoFE/trunk/CopyableMessageBox.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace S3PIDemoFE
+{
+    partial class CopyableMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpButtons = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.lbIcon = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flpButtons
+            // 
+            this.flpButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flpButtons.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flpButtons.Location = new System.Drawing.Point(0, 81);
+            this.flpButtons.Name = "flpButtons";
+            this.flpButtons.Padding = new System.Windows.Forms.Padding(6);
+            this.flpButtons.Size = new System.Drawing.Size(337, 42);
+            this.flpButtons.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tbMessage, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbIcon, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(337, 81);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbMessage.Location = new System.Drawing.Point(92, 12);
+            this.tbMessage.Margin = new System.Windows.Forms.Padding(12);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ReadOnly = true;
+            this.tbMessage.Size = new System.Drawing.Size(233, 57);
+            this.tbMessage.TabIndex = 0;
+            this.tbMessage.Text = "Text here\r\nmore text\r\nmo\r\nre\r\ntext";
+            // 
+            // lbIcon
+            // 
+            this.lbIcon.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lbIcon.AutoSize = true;
+            this.lbIcon.BackColor = System.Drawing.SystemColors.Info;
+            this.lbIcon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbIcon.Font = new System.Drawing.Font("Lucida Console", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIcon.ForeColor = System.Drawing.Color.Red;
+            this.lbIcon.Location = new System.Drawing.Point(16, 16);
+            this.lbIcon.Margin = new System.Windows.Forms.Padding(16);
+            this.lbIcon.Name = "lbIcon";
+            this.lbIcon.Size = new System.Drawing.Size(48, 45);
+            this.lbIcon.TabIndex = 1;
+            this.lbIcon.Text = "X";
+            // 
+            // CopyableMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 123);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.flpButtons);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(345, 147);
+            this.Name = "CopyableMessageBox";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CopyableMessageBox";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpButtons;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Label lbIcon;
+
+    }
+}
---
+++ S3PIDemoFE/trunk/CopyableMessageBox.cs
@@ -0,0 +1,187 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2009 by Peter L Jones                                    *
+ *  pljones@users.sf.net                                                   *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace S3PIDemoFE
+{
+    public partial class CopyableMessageBox : Form
+    {
+        public static int Show(string message)
+        {
+            return Show(message, Application.OpenForms.Count > 0 ? Application.OpenForms[0].Text : "",
+                CopyableMessageBoxIcon.Information, new List<string>(new string[] { "OK" }), -1, -1);
+        }
+        public static int Show(string message, string caption)
+        {
+            return Show(message, caption,
+                CopyableMessageBoxIcon.Information, new List<string>(new string[] { "OK" }), -1, -1);
+        }
+        public static int Show(string message, string caption, CopyableMessageBoxButtons buttons)
+        {
+            return Show(message, caption,
+                CopyableMessageBoxIcon.Information, enumToList(buttons), -1, -1);
+        }
+        public static int Show(string message, string caption, CopyableMessageBoxButtons buttons, CopyableMessageBoxIcon icon)
+        {
+            return Show(message, caption, icon, enumToList(buttons), -1, -1);
+        }
+        public static int Show(string message, string caption, CopyableMessageBoxButtons buttons, CopyableMessageBoxIcon icon, int defBtn)
+        {
+            return Show(message, caption, icon, enumToList(buttons), defBtn, -1);
+        }
+        public static int Show(string message, string caption, CopyableMessageBoxButtons buttons, CopyableMessageBoxIcon icon, int defBtn, int cncBtn)
+        {
+            return Show(message, caption, icon, enumToList(buttons), defBtn, cncBtn);
+        }
+
+        private static IList<string> enumToList(CopyableMessageBoxButtons buttons)
+        {
+            switch (buttons)
+            {
+                case CopyableMessageBoxButtons.OKCancel: return new List<string>(new string[] { "&OK", "&Cancel", });
+                case CopyableMessageBoxButtons.AbortRetryIgnore: return new List<string>(new string[] { "&Abort", "&Retry", "&Ignore", });
+                case CopyableMessageBoxButtons.YesNoCancel: return new List<string>(new string[] { "&Yes", "&No", "&Cancel", });
+                case CopyableMessageBoxButtons.YesNo: return new List<string>(new string[] { "&Yes", "&No", });
+                case CopyableMessageBoxButtons.OK:
+                default: return new List<string>(new string[] { "&OK", });
+            }
+        }
+
+        public static int Show(string message, string caption, CopyableMessageBoxIcon icon, IList<string> buttons, int defBtn, int cncBtn)
+        {
+            return Show(Application.OpenForms[0], message, caption, icon, buttons, defBtn, cncBtn);
+        }
+
+        public static int Show(IWin32Window owner, string message, string caption, CopyableMessageBoxIcon icon, IList<string> buttons, int defBtn, int cncBtn)
+        {
+            CopyableMessageBox cmb = new CopyableMessageBox(message, caption, icon, buttons, defBtn, cncBtn);
+            cmb.theButton = null;
+            cmb.ShowDialog(owner);
+            return (cmb.theButton != null) ? buttons.IndexOf(cmb.theButton.Text) : -1;
+        }
+
+
+        private CopyableMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        private CopyableMessageBox(string message, string caption, CopyableMessageBoxIcon icon, IList<string> buttons, int defBtn, int cncBtn)
+            : this()
+        {
+            if (buttons.Count < 1)
+                throw new ArgumentLengthException("At least one button text must be supplied");
+
+            this.SuspendLayout();
+            this.Text = caption;
+
+
+            Label lb = new Label();
+            lb.MaximumSize = new Size((int)(Application.OpenForms[0].Height * .8) - 90, (int)(Application.OpenForms[0].Width * .8) - 112);
+            lb.AutoSize = true;
+            lb.Text = message;
+
+            string[] lines = message.Split('\n');
+            this.tbMessage.Lines = lines;
+
+            Size tbSize = new Size(lb.PreferredHeight, lb.PreferredWidth);
+            this.Size = new Size(tbSize.Height + 90, tbSize.Width + 112);
+            
+            CreateButtons(buttons, defBtn, cncBtn);
+
+            switch (icon)
+            {
+                case CopyableMessageBoxIcon.Information:
+                    lbIcon.Visible = true; lbIcon.Text = "i"; lbIcon.ForeColor = Color.Blue; lbIcon.Font = new Font(lbIcon.Font, FontStyle.Italic); break;
+                case CopyableMessageBoxIcon.Question:
+                    lbIcon.Visible = true; lbIcon.Text = "?"; lbIcon.ForeColor = Color.Green; lbIcon.Font = new Font(lbIcon.Font, FontStyle.Regular); break;
+                case CopyableMessageBoxIcon.Warning:
+                    lbIcon.Visible = true; lbIcon.Text = "!"; lbIcon.ForeColor = Color.Brown; lbIcon.Font = new Font(lbIcon.Font, FontStyle.Bold); break;
+                case CopyableMessageBoxIcon.Error:
+                    lbIcon.Visible = true; lbIcon.Text = "X"; lbIcon.ForeColor = Color.Red; lbIcon.Font = new Font(lbIcon.Font, FontStyle.Bold); break;
+                case CopyableMessageBoxIcon.None:
+                default:
+                    lbIcon.Visible = false; break;
+            }
+
+            this.ResumeLayout();
+        }
+
+        void CreateButtons(IList<string> buttons, int defBtn, int cncBtn)
+        {
+            if (defBtn == -1) defBtn = 0;
+            if (cncBtn == -1) cncBtn = buttons.Count - 1;
+            flpButtons.SuspendLayout();
+            flpButtons.Controls.Clear();
+            for (int i = buttons.Count; i > 0; i--)
+            {
+                Button btn = CreateButton("button" + i, i, buttons[i - 1]);
+                flpButtons.Controls.Add(btn);
+                if (i == defBtn - 1) this.AcceptButton = btn;
+                if (i == cncBtn - 1) this.CancelButton = btn;
+            }
+            flpButtons.ResumeLayout();
+        }
+
+        Button CreateButton(string Name, int TabIndex, string Text)
+        {
+            Button newButton = new Button();
+            newButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            newButton.Name = Name;
+            newButton.Size = new System.Drawing.Size(75, 23);
+            newButton.TabIndex = TabIndex;
+            newButton.Text = Text;
+            newButton.UseVisualStyleBackColor = true;
+            newButton.Click += new System.EventHandler(this.button_Click);
+            return newButton;
+        }
+
+        Button theButton = null;
+        private void button_Click(object sender, EventArgs e)
+        {
+            theButton = sender as Button;
+            this.Close();
+        }
+    }
+
+    public enum CopyableMessageBoxIcon
+    {
+        None,
+        Information,
+        Question,
+        Warning,
+        Error,
+    }
+
+    public enum CopyableMessageBoxButtons
+    {
+        OK,
+        OKCancel,
+        AbortRetryIgnore,
+        YesNoCancel,
+        YesNo,
+        RetryCancel,
+    }
+
+}
