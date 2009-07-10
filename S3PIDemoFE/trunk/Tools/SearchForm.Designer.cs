bb5dd5bf2159c86cf35a2b83f7397596dabafee2
pljones <pljones@da88ef80-c9c6-43c1-8b9e-c1dffb33cf72>
2009-07-10 17:04:57 +0000

    New search form

---
+++ S3PIDemoFE/trunk/Tools/SearchForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace S3PIDemoFE.Tools
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbHex = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lbxHits = new System.Windows.Forms.ListBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnClose.Location = new System.Drawing.Point(557, 275);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbHex, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSearch, 2, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(620, 32);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter space-separated, paired &hex digits";
+            // 
+            // tbHex
+            // 
+            this.tbHex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbHex.Location = new System.Drawing.Point(205, 4);
+            this.tbHex.Name = "tbHex";
+            this.tbHex.Size = new System.Drawing.Size(331, 20);
+            this.tbHex.TabIndex = 2;
+            this.tbHex.TextChanged += new System.EventHandler(this.tbHex_TextChanged);
+            this.tbHex.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbHex_KeyPress);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Location = new System.Drawing.Point(542, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "&Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lbxHits
+            // 
+            this.lbxHits.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxHits.FormattingEnabled = true;
+            this.lbxHits.IntegralHeight = false;
+            this.lbxHits.Location = new System.Drawing.Point(12, 50);
+            this.lbxHits.Name = "lbxHits";
+            this.lbxHits.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbxHits.Size = new System.Drawing.Size(620, 219);
+            this.lbxHits.TabIndex = 2;
+            this.lbxHits.SelectedIndexChanged += new System.EventHandler(this.lbxHits_SelectedIndexChanged);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.Enabled = false;
+            this.btnCopy.Location = new System.Drawing.Point(12, 275);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "&Copy TGIs";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Enabled = false;
+            this.btnGo.Location = new System.Drawing.Point(93, 275);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "&Go to";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 301);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(644, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Visible = false;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Visible = false;
+            // 
+            // SearchForm
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(644, 323);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.lbxHits);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnClose);
+            this.Name = "SearchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Package Search";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbHex;
+        private System.Windows.Forms.ListBox lbxHits;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
---
+++ S3PIDemoFE/trunk/Tools/SearchForm.cs
@@ -0,0 +1,284 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2009 by Peter L Jones                                    *
+ *  pljones@users.sf.net                                                   *
+ *                                                                         *
+ *  This file is part of the Sims 3 Package Interface (s3pi)               *
+ *                                                                         *
+ *  s3pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s3pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using s3pi.Interfaces;
+
+namespace S3PIDemoFE.Tools
+{
+    public partial class SearchForm : Form
+    {
+        public SearchForm()
+        {
+            InitializeComponent();
+            lbxHits.Items.Clear();
+        }
+
+        #region Search form signal to main app
+        [Browsable(true)]
+        [Category("Action")]
+        [Description("Raised when the user clicks \"Go to\"")]
+        public event EventHandler<GoEventArgs> Go;
+
+        public class GoEventArgs : EventArgs
+        {
+            IResourceIndexEntry rie;
+            public GoEventArgs(IResourceIndexEntry rie) { this.rie = rie; }
+            public IResourceIndexEntry ResourceIndexEntry { get { return rie; } }
+        }
+
+        protected virtual void OnGo(object sender, GoEventArgs e) { if (Go != null) Go(sender, e); }
+        #endregion
+
+        IPackage pkg;
+        public IPackage CurrentPackage { get { return pkg; } set { pkg = value; btnSearch.Enabled = (pkg != null && tbHex.Text.Length > 0); } }
+
+        string fromRIE(IResourceIndexEntry rie) { return "0x" + rie.ResourceType.ToString("X8") + " 0x" + rie.ResourceGroup.ToString("X8") + " 0x" + rie.Instance.ToString("X16"); }
+
+        #region Search thread
+        List<IResourceIndexEntry> lrie;
+        Thread searchThread;
+        bool searching;
+        void StartSearch()
+        {
+            lrie = new List<IResourceIndexEntry>();
+            lbxHits.Items.Clear();
+            this.SearchComplete += new EventHandler<BoolEventArgs>(SearchForm_SearchComplete);
+
+            string[] input = tbHex.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            byte[] target = new byte[input.Length];
+            for (int i = 0; i < input.Length; i++) target[i] = Convert.ToByte(input[i], 16);
+
+            SearchThread st = new SearchThread(this, pkg, target, Add, updateProgress, stopSearch, OnSearchComplete);
+
+            searchThread = new Thread(new ThreadStart(st.Search));
+            searching = true;
+            searchThread.Start();
+        }
+
+        void SearchForm_SearchComplete(object sender, SearchForm.BoolEventArgs e)
+        {
+            searching = false;
+            while (searchThread != null && searchThread.IsAlive)
+                searchThread.Join(100);
+            searchThread = null;
+
+            this.toolStripProgressBar1.Visible = false;
+            this.toolStripStatusLabel1.Visible = false;
+
+            tbHex.Enabled = true;
+            btnSearch.Text = "&Search";
+        }
+
+        void AbortSearch()
+        {
+            if (!searching) SearchForm_SearchComplete(null, new BoolEventArgs(false));
+            else searching = false;
+        }
+
+        delegate void AddCallBack(IResourceIndexEntry rie);
+        void Add(IResourceIndexEntry rie)
+        {
+            lbxHits.Items.Add(fromRIE(rie));
+            lrie.Add(rie);
+        }
+
+        class BoolEventArgs : EventArgs
+        {
+            public bool arg;
+            public BoolEventArgs(bool arg) { this.arg = arg; }
+        }
+        event EventHandler<BoolEventArgs> SearchComplete;
+        delegate void searchCompleteCallback(bool complete);
+        void OnSearchComplete(bool complete) { if (SearchComplete != null) { SearchComplete(this, new BoolEventArgs(complete)); } }
+
+        delegate void updateProgressCallback(bool changeText, string text, bool changeMax, int max, bool changeValue, int value);
+        void updateProgress(bool changeText, string text, bool changeMax, int max, bool changeValue, int value)
+        {
+            if (changeText)
+            {
+                toolStripStatusLabel1.Visible = text.Length > 0;
+                toolStripStatusLabel1.Text = text;
+            }
+            if (changeMax)
+            {
+                if (max == -1)
+                    toolStripProgressBar1.Visible = false;
+                else
+                {
+                    toolStripProgressBar1.Visible = true;
+                    toolStripProgressBar1.Maximum = max;
+                }
+            }
+            if (changeValue)
+                toolStripProgressBar1.Value = value;
+        }
+
+        public delegate bool stopSearchCallback();
+        private bool stopSearch() { return !searching; }
+
+        class SearchThread
+        {
+            SearchForm form;
+            IPackage pkg;
+            byte[] pattern;
+            AddCallBack addCB;
+            updateProgressCallback updateProgressCB;
+            stopSearchCallback stopSearchCB;
+            searchCompleteCallback searchCompleteCB;
+
+            public SearchThread(SearchForm form, IPackage pkg, byte[] pattern,
+                AddCallBack addCB, updateProgressCallback updateProgressCB, stopSearchCallback stopSearchCB, searchCompleteCallback searchCompleteCB)
+            {
+                this.form = form;
+                this.pkg = pkg;
+                this.pattern = (byte[])pattern.Clone();
+                this.addCB = addCB;
+                this.updateProgressCB = updateProgressCB;
+                this.stopSearchCB = stopSearchCB;
+                this.searchCompleteCB = searchCompleteCB;
+            }
+
+            public void Search()
+            {
+                bool complete = false;
+                try
+                {
+                    updateProgress(true, "Retrieving resource index...", true, 0, true, 0);
+                    IList<IResourceIndexEntry> lrie = pkg.GetResourceList;
+                    if (stopSearch) return;
+
+                    updateProgress(true, "Starting search... 0%", true, lrie.Count, true, 0);
+                    int freq = Math.Max(1, lrie.Count / 100);
+
+                    int hits = 0;
+                    for (int i = 0; i < lrie.Count; i++)
+                    {
+                        if (stopSearch) return;
+
+                        if (search(lrie[i]))
+                        {
+                            Add(lrie[i]);
+                            hits++;
+                        }
+
+                        if (i % freq == 0)
+                            updateProgress(true, String.Format("[Hits {0}] Searching... {1}%", hits, i * 100 / lrie.Count), false, -1, true, i);
+                    }
+                    complete = true;
+                }
+                catch (ThreadInterruptedException) { }
+                finally
+                {
+                    updateProgress(true, "Search ended", true, 0, true, 0);
+                    searchComplete(complete);
+                }
+            }
+
+            bool search(IResourceIndexEntry rie)
+            {
+                IResource res = s3pi.WrapperDealer.WrapperDealer.GetResource(0, pkg, rie, true);// we're searching bytes
+                byte[] resBytes = res.AsBytes;
+                for (int i = 0; i < resBytes.Length - pattern.Length; i++)
+                {
+                    for (int j = 0; j < pattern.Length; j++)
+                        if (resBytes[i + j] != pattern[j]) goto fail;
+                    return true;
+                fail: { }
+                }
+                return false;
+            }
+
+            void Add(IResourceIndexEntry rie) { Thread.Sleep(0); if (form.IsHandleCreated) form.Invoke(addCB, new object[] { rie, }); }
+
+            void updateProgress(bool changeText, string text, bool changeMax, int max, bool changeValue, int value)
+            {
+                Thread.Sleep(0);
+                if (form.IsHandleCreated) form.Invoke(updateProgressCB, new object[] { changeText, text, changeMax, max, changeValue, value, });
+            }
+
+            bool stopSearch { get { Thread.Sleep(0); return !form.IsHandleCreated || (bool)form.Invoke(stopSearchCB); } }
+
+            void searchComplete(bool complete) { Thread.Sleep(0); if (form.IsHandleCreated) form.BeginInvoke(searchCompleteCB, new object[] { complete, }); }
+        }
+        #endregion
+
+        #region Form control event handlers
+        static string valid = "0123456789abcdefABCDEF ";
+        private void tbHex_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 32 && e.KeyChar < 0x7f && valid.IndexOf(e.KeyChar) < 0) e.Handled = true;
+        }
+
+        Regex rx = new Regex(@"^[0-9A-F][0-9A-F]?(\s+([0-9A-F][0-9A-F]?)?)*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private void tbHex_TextChanged(object sender, EventArgs e)
+        {
+            if (!rx.IsMatch(tbHex.Text))
+            {
+                if (tbHex.SelectionStart > 0) tbHex.SelectionStart--;
+                tbHex.SelectionLength = Math.Min(1, tbHex.Text.Length);
+                if (tbHex.SelectionLength > 0) tbHex.SelectedText = "";
+            }
+            else btnSearch.Enabled = tbHex.Text.Length > 0;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (searching)
+                AbortSearch();
+            else
+            {
+                tbHex.Enabled = false;
+                btnSearch.Text = "&Stop";
+                StartSearch();
+            }
+        }
+
+        private void lbxHits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnGo.Enabled = lbxHits.SelectedItems.Count == 1;
+            btnCopy.Enabled = lbxHits.SelectedItems.Count > 0;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            foreach (int i in lbxHits.SelectedIndices) s += fromRIE(lrie[i]) + "\n";
+            //s = s.TrimEnd('\n');
+            Clipboard.SetText(s);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            OnGo(this, new GoEventArgs(lrie[lbxHits.SelectedIndex]));
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
